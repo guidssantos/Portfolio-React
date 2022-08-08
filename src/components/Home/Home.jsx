0a2b481193d45e3be483f0deb5ce74f21595ffb2
Guilhermeaka <guilhermedssantos1@gmail.com>
2022-08-08 15:58:10 -0300

    Troca de link curriculo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ export default function Home(){
                             }}/>  
                         </h2>                        
                         <ButtonHome>                       
-                        <a href='https://drive.google.com/u/1/uc?id=1g78iQo6Xco7IDCBOdmRWYfAE14dOPzXp&export=download'><span><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z" fill="currentColor"></path></svg> Baixar Curriculo </span></a>
+                        <a href='https://drive.google.com/u/1/uc?id=1c5RDTz5Hfr3kzLIG46d64_joFl1Cy2RG&export=download'><span><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z" fill="currentColor"></path></svg> Baixar Curriculo </span></a>
                         </ButtonHome>
                     </Perfil> 
                     <PerfilImage >
